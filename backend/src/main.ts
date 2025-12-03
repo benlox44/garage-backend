8e1a082a838f3e0b5a29aa72d2284d2bf031d038
atuning120 <90119017+atuning120@users.noreply.github.com>
2025-12-03 15:51:39 -0300

    ohoho

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap(): Promise<void> {
 
   const clientOrigin = required('CLIENT_URL');
   app.enableCors({
-    origin: [clientOrigin],
+    origin: clientOrigin.split(',').map((origin) => origin.trim()),
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
     credentials: true,
